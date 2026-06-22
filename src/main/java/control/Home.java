0bdf59aed0b32fc5cac5a58558179c5f08ac9288
Diego <d.bottiglieri@studenti.unisa.it>
2026-06-22 17:06:31 +0200

    Configurazione iniziale grafica: collegato style.css e aggiunta immagine di sfondo alla home

---
+++ src/main/java/control/Home.java
@@ -0,0 +1,34 @@
+package control;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Questo comando dice a Tomcat di prendere segretamente la JSP dentro WEB-INF e mostrarla all'utente
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
